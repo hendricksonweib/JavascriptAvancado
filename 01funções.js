ef986df7ff9116411c8f66b5329f62cd5700bdf8
hendricksonrocha22@gmail.com <hendricksonweib>
2023-11-24 14:33:03 -0300

    first commit

---
+++ 01funções.js
@@ -0,0 +1,7 @@
+function sayHello(name, lastName){
+    console.log(`Olá ${name} ${lastName}. Bem-vindo(a) á Dev Samurai!`) 
+}
+
+sayHello("Felipe","Fontura")
+sayHello("Paulo","Siqueira")
+sayHello("Marcelo","Silva")
---
+++ 02retorno.js
@@ -0,0 +1,4 @@
+function sum(n1, n2){
+    return n1 + n2
+}
+console.log(sun(1,1))
