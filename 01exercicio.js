37e7c04aab7c8a67d9a60ce64817797d624ebbe4
hendricksonweib <hendricksonrocha22@gmail.com>
2023-11-25 16:06:38 -0300

    exercicio ok
    Signed-off-by: hendricksonweib <hendricksonrocha22@gmail.com>

--- 01exercicio.js
+++ 01exercicio.js
@@ -9,3 +9,13 @@ console.log(soma(1,5))
 
 // Exercício 2
 // Crie uma função que receba um objeto como parâmetro e retorne um novo objeto com os valores do objeto original multiplicados por dois.
+
+function sum(){
+   let res = 0
+ for(n of arguments){
+res = n
+ }
+ return res*2
+}
+
+console.log(sum(1,4,6,7,8,9,6,6,5,2,4,3,0,2,6,4,5,2,3,4,8,5,9,7,))
