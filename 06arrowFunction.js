82ec97693cbec8d60c0b6048be739ab1dd7b6213
hendricksonweib <hendricksonrocha22@gmail.com>
2023-11-28 09:07:31 -0300

    aula 6
    Signed-off-by: hendricksonweib <hendricksonrocha22@gmail.com>

---
+++ 06arrowFunction.js
@@ -0,0 +1,5 @@
+const mult = (n) =>(n*=n)
+console.log(mult(10))
+
+//arrow function é uma forma de declara função de forma curta, ou seja para sintaxes menores. 
+//As arrow Function elas tem um auto return ou seja ela sem as { } ela se auto retorna, mas se usar as chaves OBRIGATORIAMENTE, necessita do return
