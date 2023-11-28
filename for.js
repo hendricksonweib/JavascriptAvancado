1a458f27f96903e0a77e7445106591109870abc6
hendricksonweib <hendricksonrocha22@gmail.com>
2023-11-28 09:07:48 -0300

    revisão sobre for
    Signed-off-by: hendricksonweib <hendricksonrocha22@gmail.com>

---
+++ for.js
@@ -0,0 +1,14 @@
+for(let i = 11;i>-1 ;i--){
+console.log(i)
+}
+console.log('acabou')
+
+//for é uma função que percorre determinados parementros
+// for ( ; ; ){
+
+// }
+//primeiro argumento do for é o seu ponto inicial onde você deve declara sua variável, ou seja a sua condição inicial, ou seja você diz pro for assim "olha eu quero que você comece aqui viu!"
+
+//na segunda parte é a condição ou seja você diz pro meio que "olha for eu quero que você vá do primeiro ponto até aqui viu!"
+
+// na terceira parte é onde você diz como você quer que o for chegue até o seu ponto final ou seja você pode dizer "ah for eu quero que você chegue la indo de 1 em 1 ou indo de 5 em 5..."por ai vai as possibilidades.
